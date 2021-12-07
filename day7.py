575cbbf05b79a79ed67d17e6059586198743c880
Nat Peterson <natpeterson@gmail.com>
2021-12-07 08:19:41 -0800

    day7 tidy

--- day7.py
+++ day7.py
@@ -6,45 +6,54 @@ puzzle = readinput(7)[0]
 
 crabs = [int(x) for x in puzzle.split(",")]
 
-# find average as a starting point
-average = sum(crabs)//len(crabs)
-print(f"average {average}")
+fuel_memo = dict()
 
 def fuel(crabs, goto):
-    return sum([abs(crab - goto) for crab in crabs])
+    global fuel_memo
+    if goto not in fuel_memo:        
+        fuel_memo[goto] = sum([abs(crab - goto) for crab in crabs])
+    return fuel_memo[goto]
 
-min_fuel = None
-best_goto = None
-for goto in range(min(crabs), max(crabs)):
-    goto_fuel = fuel(crabs,goto)
-    #print(f"goto {goto} fuel {goto_fuel}")
-    if not min_fuel or goto_fuel < min_fuel:
-        min_fuel = goto_fuel
-        best_goto = goto
-    if goto_fuel > min_fuel:
-        break
-
-print("#1",min_fuel, best_goto)
-
+fuel2_memo = dict()
 
 def fuel2(crabs, goto):
-    # 5 = 1+2+3+4+5 => n*(n+1)/2
-    total_fuel = 0
-    for crab in crabs:
-        n = abs(crab - goto)
-        total_fuel += n*(n+1)//2
+    global fuel2_memo
+    if goto not in fuel2_memo:     
+        # 5 = 1+2+3+4+5 => n*(n+1)/2
+        total_fuel = 0
+        for crab in crabs:
+            n = abs(crab - goto)
+            total_fuel += n*(n+1)//2
+        fuel2_memo[goto] = total_fuel
+    return fuel2_memo[goto]
 
-    return total_fuel
+def solver(crabs, fuel_func):
+    # find average as a starting point
+    guess = sum(crabs)//len(crabs)
 
-min_fuel = None
-best_goto = None
-for goto in range(min(crabs), max(crabs)):
-    goto_fuel = fuel2(crabs,goto)
-    #print(f"goto {goto} fuel {goto_fuel}")
-    if not min_fuel or goto_fuel < min_fuel:
-        min_fuel = goto_fuel
-        best_goto = goto
-    if goto_fuel > min_fuel:
-        break
+    min_fuel = None
+    best_goto = None
 
-print("#2",min_fuel, best_goto)
+    while True:
+        # compute fuel at this position
+        guess_fuel = fuel_func(crabs,guess)
+
+        # retain position if best so far
+        if not best_goto or guess_fuel < min_fuel:
+            min_fuel = guess_fuel
+            best_goto = guess
+
+        # if +1/-1 is better, go there
+        if fuel_func(crabs, guess+1) < min_fuel:
+            guess = guess+1
+        elif fuel_func(crabs, guess-1) < min_fuel:
+            guess = guess-1
+        else:
+            # we are done
+            break
+
+    return (min_fuel, best_goto)
+
+print('#1', solver(crabs, fuel)[0])
+    
+print('#2', solver(crabs, fuel2)[0])
