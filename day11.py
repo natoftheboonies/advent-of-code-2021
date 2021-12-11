3b970efa2f3cde6d45ed5a3be7e7d5d1f3c53ac3
Nat Peterson <natpeterson@gmail.com>
2021-12-11 08:25:06 -0800

    fixup! day11 octopi

--- day11.py
+++ day11.py
@@ -1,5 +1,4 @@
 from aoc import readinput
-from collections import deque
 from itertools import count
 
 puzzle = """11111
@@ -31,7 +30,7 @@ def print_octopi(octopi):
     for y, line in enumerate(puzzle):
         for x, _ in enumerate(line):
             print(grid[(x, y)], end='')
-        print('')
+        print()
     print()
 
 
@@ -44,38 +43,41 @@ all_flashed = 0
 for step in count(1):
 
     # increment all
-    to_flash = deque()
-    for k in grid.keys():
+    to_flash = set()
+    for k in grid:
         grid[k] += 1
         # collect flash
         if grid[k] > 9:
-            to_flash.append(k)
+            to_flash.add(k)
 
     flashed = set()
     while to_flash:
         flash_me = to_flash.pop()
+        # increment neighbors
         for d in DIRS:
             neighbor = (flash_me[0]+d[0], flash_me[1]+d[1])
             if neighbor in grid:
-                if neighbor in flashed or neighbor in to_flash:
+                # nobody re-flashes
+                if neighbor in flashed:
                     continue
                 grid[neighbor] += 1
                 if grid[neighbor] > 9:
-                    to_flash.append(neighbor)
+                    to_flash.add(neighbor)
         flashed.add(flash_me)
 
     for octopus in flashed:
         grid[octopus] = 0
 
+    # part 1, count flashes
     if step < 100:
         total_flashes += len(flashed)
 
+    # part 2, find all flashed
     if len(flashed) == len(grid):
         all_flashed = step+1  # start step 1 not 0
         break
 
-
-# print_octopi(grid)
+#print_octopi(grid)
 
 print('#1', total_flashes)
 print('#2', all_flashed)
