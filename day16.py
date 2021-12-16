aa38b030e62323c82a75307f7ba76bbbe6ae0a57
Nat Peterson <natpeterson@gmail.com>
2021-12-16 08:48:59 -0800

    day16 tidy

--- day16.py
+++ day16.py
@@ -1,6 +1,5 @@
 from aoc import readinput
 
-
 rules = """0 = 0000
 1 = 0001
 2 = 0010
@@ -18,81 +17,64 @@ D = 1101
 E = 1110
 F = 1111""".splitlines()
 
-hex2bin = {}
+hex_rules = {}
 for line in rules:
     left, right = line.split(" = ")
-    hex2bin[left] = right
+    hex_rules[left] = right
 
 
-def hex_to_bin(input):
+def hex2bin(input):
     result = ""
     for char in input:
-        result += hex2bin[char]
+        result += hex_rules[char]
     return result
 
 
-version_sum = 0
+def read_packet(packet, ptr=0):
 
-
-def packet_decoder(packet, ptr=0):
-    """well then"""
     global version_sum
 
-    def decode_literal(stream, ptr):
+    ver = int(packet[ptr:ptr+3], 2)
+    version_sum += ver
+
+    type = int(packet[ptr+3:ptr+6], 2)
+    assert type in range(8)
+    ptr += 6
+
+    if type == 4:
+        # literal
         bits = ''
         while True:
-            group = stream[ptr:ptr+5]
+            group = packet[ptr:ptr+5]
             ptr += 5
             bits += group[1:]
             if group[0] == '0':
                 break
-        literal = int(bits, 2)
-        return literal, ptr
-
-    def decode_header(stream, ptr):
-        # print(stream)
-        # literal packet
-        ver = int(stream[ptr:ptr+3], 2)
-        ptr += 3
-        type = int(stream[ptr:ptr+3], 2)
-        ptr += 3
-        stream = stream[3:]
-        return (ver, type), ptr
-
-    # if ptr > len(packet):
-    #     return (0,), ptr
-
-    value = 0
-
-    (ver, type), ptr = decode_header(packet, ptr)
-    print(f"ver {ver}")
-    version_sum += ver
-    if type == 4:
-        literal, ptr = decode_literal(packet, ptr)
-        print(f"lit {literal}")
-        value = literal
+        value = int(bits, 2)
     else:
         # operators with sub-packets
         sub_packets = []
         length_type = int(packet[ptr])
         ptr += 1
-        print(f"op type {type}-{length_type}")
         if length_type == 0:
             # 15-bit number indicating length of subpackets
             len_subs = int(packet[ptr:ptr+15], 2)
             ptr += 15
             end = ptr+len_subs
             while ptr < end:
-                sub_value, ptr = packet_decoder(packet, ptr)
+                sub_value, ptr = read_packet(packet, ptr)
                 sub_packets.append(sub_value)
-
         elif length_type == 1:
             # 11-bit number representing the number of sub-packets
             num_subs = int(packet[ptr:ptr+11], 2)
             ptr += 11
             for _ in range(num_subs):
-                sub_value, ptr = packet_decoder(packet, ptr)
+                sub_value, ptr = read_packet(packet, ptr)
                 sub_packets.append(sub_value)
+
+        else:
+            raise RuntimeError(
+                f"Unexpected length_type {length_type} at {ptr-1}")
 
         # now do operator math
         if type == 0:
@@ -121,11 +103,17 @@ def packet_decoder(packet, ptr=0):
             # equal to
             assert len(sub_packets) == 2
             value = 1 if sub_packets[0] == sub_packets[1] else 0
+        else:
+            raise RuntimeError(
+                f"Unexpected operator {type}")
 
     return value, ptr
 
 
 puzzle = readinput(16)[0]
-result = packet_decoder(hex_to_bin(puzzle))
+
+# part1, record sum of versions as we explore packets
+version_sum = 0
+result, ptr = read_packet(hex2bin(puzzle))
 print("#1", version_sum)
-print("#2", result[0])
+print("#2", result)
