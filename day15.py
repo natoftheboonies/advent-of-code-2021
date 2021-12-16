27bb14ad1895ba881fcc253da4db102522fbd090
Nat Peterson <natpeterson@gmail.com>
2021-12-15 17:27:34 -0800

    day15 tidy

--- day15.py
+++ day15.py
@@ -1,5 +1,4 @@
 from aoc import readinput
-from collections import deque
 import heapq
 
 puzzle = """1163751742
@@ -22,81 +21,55 @@ for y, line in enumerate(puzzle):
     for x, char in enumerate(line):
         maze[(x, y)] = int(char)
 
-start = (0, 0)
 goal = max(maze)
 
 # hmm, what are those nice search algorithms?
 # dijkstra's: https://www.youtube.com/watch?v=pVfj6mxhdMw
 
-shortest = {start: 0}
 
-h = []
-heapq.heappush(h, (0, (0, 0)))
-
-while h:
-    base_cost, last = heapq.heappop(h)
-    x, y = last
-    if x > goal[0] or x < 0 or y > goal[1] or y < 0:
-        continue
-    if last == goal:
-        break
-    neighbors = dict()
-    # calc distance of each neighbor
-    for d in DIRS:
-        x, y = last[0]+d[0], last[1]+d[1]
-        if (x, y) not in maze:
-            continue
-        neighbor_cost = base_cost + maze[(x, y)]
-        if shortest.get((x, y), 99999) > neighbor_cost:
-            shortest[(x, y)] = neighbor_cost
-            heapq.heappush(h, (neighbor_cost, (x, y)))
-
-print("#1", base_cost)
-
-print(goal)
-
-
-def part2_cost(node):
-    #global goal
+def calc_cost(node):
+    """cost is base cost plus number of tiles right and down"""
     x, y = node
     base_x = x % (goal[0]+1)
-    #print(f"{base_x} for {x}")
     mult_x = x // (goal[0]+1)
     base_y = y % (goal[1]+1)
     mult_y = y // (goal[1]+1)
-    #print(maze[(base_x, base_y)], base_x, mult_x)
     cost = maze[(base_x, base_y)] + mult_x + mult_y
+    # wrap cost after 9 to 1
     if cost > 9:
         cost -= 9
     return cost
 
 
-#assert part2_cost((49, 49)) == 9
+def dijkstra(mult=1):
 
-goal2 = tuple((x+1)*5-1 for x in goal)
-print(goal2)
+    max_maze = max(maze)
+    goal = tuple((x+1)*mult-1 for x in max_maze)
 
-shortest = {start: 0}
+    shortest = {(0, 0): 0}
 
-h2 = []
-heapq.heappush(h2, (0, (0, 0)))
+    h = []
+    heapq.heappush(h, (0, (0, 0)))
 
-while h2:
-    base_cost, last = heapq.heappop(h2)
-    x, y = last
-    if x > goal2[0] or x < 0 or y > goal2[1] or y < 0:
-        continue
-    if last == goal2:
-        break
-    neighbors = dict()
-    # calc distance of each neighbor
-    for d in DIRS:
-        x, y = last[0]+d[0], last[1]+d[1]
-        if x > goal2[0] or x < 0 or y > goal2[1] or y < 0:
+    while h:
+        base_cost, last = heapq.heappop(h)
+        x, y = last
+        if x > goal[0] or x < 0 or y > goal[1] or y < 0:
             continue
-        neighbor_cost = base_cost + part2_cost((x, y))
-        if shortest.get((x, y), 999999) > neighbor_cost:
-            shortest[(x, y)] = neighbor_cost
-            heapq.heappush(h2, (neighbor_cost, (x, y)))
+        if last == goal:
+            return base_cost
+        # calc distance of each neighbor
+        for d in DIRS:
+            x, y = last[0]+d[0], last[1]+d[1]
+            if x > goal[0] or x < 0 or y > goal[1] or y < 0:
+                continue
+            neighbor_cost = base_cost + calc_cost((x, y))
+            if shortest.get((x, y), 99999) > neighbor_cost:
+                shortest[(x, y)] = neighbor_cost
+                heapq.heappush(h, (neighbor_cost, (x, y)))
+    raise RuntimeError(f"never reached goal {goal}")
 
-print("#2", base_cost)
+
+print("#1", dijkstra())
+
+print("#2", dijkstra(5))
