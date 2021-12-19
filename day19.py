1dfc177e61aa8faf6bae677cf92dce4cef3853b3
Nat Peterson <natpeterson@gmail.com>
2021-12-19 13:19:04 -0800

    day19 i suck

--- day19.py
+++ day19.py
@@ -26,7 +26,7 @@ def parse(puzzle):
             scanner = re.findall("\d+", line)[0]
             points = []
         else:
-            points.append(([int(y) for y in re.findall("-?\d+", line)]))
+            points.append(tuple([int(y) for y in re.findall("-?\d+", line)]))
             scanners[scanner] = points
     return scanners
 
@@ -177,8 +177,7 @@ def compute_distances_3d(points):
 puzzle = readinput('19ex')
 #puzzle = readinput(19)
 scanners = parse(puzzle)
-
-
+print("total obs", sum(len(x) for x in scanners.values()))
 
 # find rotations of scanners relative to other scanner
 scanner_rots = dict()
@@ -202,9 +201,71 @@ for i, s0 in enumerate(scanner_ids):
                 scanner_rots[(s0,s)] = best_rot
                 break
 
+print("rots:")
 pprint(scanner_rots)
 
+# now what? i guess we gotta figure out *which* beacons overlap.
+# we know which pairs overlap, 
 
+scanner_pos = dict()
+
+all_points = set()
+for k,v in scanners.items():
+    for p in v:
+        all_points.add((k, p))
+
+for s0, s1 in scanner_rots:
+    rot = scanner_rots[(s0,s1)]
+    for s0_point in scanners[s0]:
+        dists = {dist_vector_3d(s0_point, p) for p in scanners[s0]}
+        matched = False
+        for s1_point in scanners[s1]: 
+            s1_dists = {rotate3d(dist_vector_3d(s1_point, p),rot) for p in scanners[s1]}
+            if len(dists.intersection(s1_dists)) >= 12:
+                #print(f"{s0_point} matches {s1_point}")
+                relative_distance = dist_vector_3d(s0_point, rotate3d(s1_point,rot))
+                scanner_pos[s0,s1] = relative_distance
+                matched = True
+
+
+
+print(scanner_pos)
+
+"""
+
+say 0 has a, b, c
+and 1 has d, e, f
+and 2 has g, h, i
+
+and further that a == d, b == f
+and d == h and e == i
+
+how do we count the uniques? 
+store 0-a, 0-b, 0-c
+1-d ? no because == 0-a
+1-e ? yes
+1-f ? no because == 0-b
+2-g ? yes
+2-h ? no because == 1-d, even though 1-d is also not counted.
+2-i ? no because == 1-e
+
+"""
+
+
+
+
+# scanner_rots['0','0'] = ROTATE_3D[0]
+
+# relative_to_s0 = {'0': (0,0,0)}
+
+# for s0, s1 in scanner_pos:
+#     rot = scanner_rots['0',s0]
+#     if s0 in relative_to_s0:
+#         relative_pos = rotate3d(scanner_pos[s1,s0],rot)
+#         print("relative_pos", relative_pos) 
+#         relative_to_s0[s1] = dist_vector_3d(relative_to_s0[s0], relative_pos)
+
+# pprint(relative_to_s0)
 
 
 
